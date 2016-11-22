2f7c7f10e2c1043996d8ba6ac77f080cce6dfef6
sreiss <simon.reiss57@gmail.com>
2016-11-22 15:13:40 +0100

    Wrong import

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -4,7 +4,6 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.inject.Inject;
-import com.sun.javaws.progress.Progress;
 import models.Kingdom;
 import services.contracts.*;
 import views.MainWindow;
